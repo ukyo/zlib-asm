f3486d0311caf3209a68b57e05e3761124adc35e
Syu Kato <ukyo.web@gmail.com>
2013-04-30 12:51:38 +0900

    Update web-bench.js

--- web-bench.js
+++ web-bench.js
@@ -1,3 +1,24 @@
 window.onload = function() {
+  var fileInput = document.querySelector('#f'),
+      result = document.querySelector('#result');
   
+  fileInput.addEventListener('change', function(e) {
+    var file = e.target.files[0];
+    var reader = new FileReader;
+    reader.readAsArrayBuffer(file);
+    reader.onload = function() {
+      bench(new Uint8Array(reader.result));
+    };
+  }, false);
+  
+  function bench(bytes) {
+    var start = Date.now();
+    var s = '<div>';
+    var comp = window[key].compress(bytes);
+    s += 'compress: ' + (Date.now() - start) + 'ms.';
+    start = Date.now();
+    window[key].decompress(comp);
+    s += ' decompress: ' + (Date.now() - start) + 'ms.</div>';
+    result.innerHTML += s;
+  }
 };
