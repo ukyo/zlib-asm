a5aba90997241881ce0fba48fce7871ef8d1a8db
ukyo <ukyo.web@gmail.com>
2013-11-30 02:22:39 +0900

    今風の書き方にした

--- post.js
+++ post.js
@@ -1,13 +1,31 @@
-    var tmp = new Uint8Array(++outputIndex);
-    tmp.set(outputBytes.subarray(0, outputIndex));
-    return tmp;
-  },
+  var $inputNode, $outputNode;
 
-  compress: function(bytes) {
-    return this.run(bytes);
-  },
+  function $run (args, input) {
+    if ($inputNode) FS.destroyNode($inputNode);
+    if ($outputNode) FS.destroyNode($outputNode);
 
-  decompress: function(bytes) {
-    return this.run(bytes, true);
+    $inputNode = FS.createDataFile(
+      '/',
+      'input',
+      input,
+      true,
+      false
+    );
+    $outputNode = FS.createDataFile(
+      '/',
+      'output',
+      new Uint8Array(0),
+      false,
+      true
+    );
+    Module.callMain(args);
+    return $outputNode.contents;
   }
-};
+
+  return {
+    run: $run,
+    compress: $run.bind(null, []),
+    decompress: $run.bind(null, ['-d'])
+  };
+
+})();
--- pre.js
+++ pre.js
@@ -7,25 +7,9 @@ if (typeof module !== 'undefined' && module.exports) {
   global = this;
 }
 
-global[key] = {
-  run: function(inputBytes, decompress) {
-    var inputIndex = -1;
-    var outputIndex = -1;
-    var outputBytes = new Uint8Array(0x8000);
-    var Module = {
-      arguments: decompress ? ['-d'] : [],
-      stdin: function() {
-        return inputBytes[++inputIndex];
-      },
-      stdout: function(x) {
-        var tmp;
-        if (x !== null) {
-          if (++outputIndex === outputBytes.length) {
-            tmp = new Uint8Array(outputBytes.length * 2);
-            tmp.set(outputBytes);
-            outputBytes = tmp;
-          }
-          outputBytes[outputIndex] = x;
-        }
-      }
-    };
+global[key] = (function () {
+  var Module = {
+    noInitialRun: true
+  };
+
+
