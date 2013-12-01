21fbc38f34719761a2e9cb40248b0e59d25baa6f
ukyo <ukyo.web@gmail.com>
2013-12-02 00:51:27 +0900

    create custom FileSystem API

--- dest/zlib-asm.js
+++ dest/zlib-asm.js
@@ -4973,62 +4973,138 @@ run();
 // {{POST_RUN_ADDITIONS}}
 // {{MODULE_ADDITIONS}}
 
-  var $inputNode, $outputNode, $originalWriteFn, $originalEnsureFlexible, $buffer;
+  // 必要なのだけ実装
+  var MYFS = {
+    // FS.mount実行時に呼ばれる  
+    mount: function (parent, name, mode, rdev) {
+      return MYFS.createNode(null, '/', 16384 | 0777, 0);
+    },
+    // 作成したノードを操作するメソッド
+    node_ops: {
+      // ノードの属性を書き込み(例: node.timestamp)
+      setattr: function (node, attr) {
+        if (attr.mode !== undefined) node.mode = attr.mode;
+        if (attr.timestamp !== undefined) node.timestamp = attr.timestamp;
+        if (attr.size !== undefined) {
+          var contents = node.contents;
+          if (contents.length > attr.size) {
+            contents = contents.subarray(0, attr.size);
+          } else {
+            contents = MYFS.expandBuffer(contents, attr.size);
+          }
+          node.contents = contents;
+          node.size = attr.size;
+        }
+      },
+      // ノードを検索するんだけど、不必要なのでMEMFSから拝借
+      lookup: MEMFS.node_ops.lookup,
+      // 新しいノードを作る
+      mknod: function (parent, name, mode, dev) {
+        return MYFS.createNode(parent, name, mode, dev);
+      }
+    },
+    // IO系の操作をするメソッド
+    stream_ops: {
+      // 読み
+      read: function (stream, buffer, offset, length, position) {
+        var node = stream.node,
+            contents = node.contents,
+            size = Math.min(contents.length - position, length);
 
-  // cache of output buffer
-  $buffer = new Uint8Array(0x8000);
+        if (size > 8 && contents.subarray) { // non-trivial, and typed array
+          buffer.set(contents.subarray(position, position + size), offset);
+        } else {
+          for (var i = 0; i < size; i++) {
+            buffer[offset + i] = contents[position + i];
+          }
+        }
+        return size;
+      },
+      // 書き
+      write: function (stream, buffer, offset, length, position, canOwn) {
+        var node = stream.node,
+            contents = node.contents,
+            bufferSize = contents.length,
+            size = position + length;
+        // expand buffer
+        if (bufferSize === 0) {
+          contents = new Uint8Array(size);
+          bufferSize = contents.length;
+        }
+        contents = MYFS.expandBuffer(contents, size);
+        // write
+        contents.set(buffer.subarray(offset, offset + length), position);
+        node.contents = contents;
+        node.size = size;
 
-  // save original functions of MEMFS
-  $originalWriteFn = MEMFS.stream_ops.write;
-  $originalEnsureFlexible = MEMFS.ensureFlexible;
+        return length;
+      }
+    },
+    // ノードの作成。node_ops, stream_ops, contentsを設定する必要がある
+    createNode: function (parent, name, mode, dev) {
+      var node = FS.createNode(parent, name, mode, dev);
+      node.node_ops = MYFS.node_ops;
+      node.stream_ops = MYFS.stream_ops;
+      node.contents = [];
+      node.timestamp = Date.now();
+      if (parent) parent.contents[name] = node;
+      return node;
+    },
+    // FS.createDataFileっぽく使えるやつ
+    createFile: function (parent, name, data, r, w) {
+      var node = FS.createFile(
+        parent,
+        name,
+        {},
+        r,
+        w
+      );
+      node.contents = data;
+      node.node_ops = MYFS.node_ops;
+      node.stream_ops = MYFS.stream_ops;
+      return node;
+    },
+    // バッファサイズの拡張。とりあえず倍々
+    expandBuffer: function (buffer, size) {
+      if (buffer.length >= size) return buffer;
+      var bufferSize = buffer.length, _buffer;
+      while (bufferSize < size) bufferSize *= 2;
+      var _buffer = new Uint8Array(bufferSize);
+      _buffer.set(buffer);
+      return _buffer;
+    }
+  };
 
-  var contentsSize;
-  function $write (stream, buffer, offset, length, position, canOwn) {
-    var bufferSize = $buffer.length, b;
-    stream.node.timestamp = Date.now();
-    
-    // resize the buffer
-    while (bufferSize < position) bufferSize *= 2;
-    b = new Uint8Array(bufferSize);
-    b.set($buffer);
-    $buffer = b;
-    $buffer.set(buffer.subarray(offset, offset + length), position);
-    
-    stream.node.contents = $buffer;
-    contentsSize = position;
-    return length;
-  }
-
-  function $noop () {}
-
+  // MYFSをマウント。絶対必要！  
+  FS.mount(MYFS, {}, '/');
 
   function $run (args, input) {
-    if ($inputNode) FS.destroyNode($inputNode);
-    if ($outputNode) FS.destroyNode($outputNode);
-    MEMFS.stream_ops.write = $write;
-    MEMFS.ensureFlexible = $noop;
-
-    $inputNode = FS.createDataFile(
+    // ファイルが残っている場合は削除
+    try {
+      FS.destroyNode(FS.lookupPath('/input').node);
+    } catch (e) {} 
+    try {
+      FS.destroyNode(FS.lookupPath('/output').node);
+    } catch (e) {}
+    // ファイルを作成
+    MYFS.createFile(
       '/',
       'input',
       input,
       true,
-      false
+      true
     );
-
-    $outputNode = FS.createDataFile(
+    var outputNode = MYFS.createFile(
       '/',
       'output',
       new Uint8Array(0),
-      false,
+      true,
       true
     );
+    // main関数呼び出し
     Module.callMain(args);
-
-    MEMFS.stream_ops.write = $originalWriteFn;
-    MEMFS.ensureFlexible = $originalEnsureFlexible;
-
-    return new Uint8Array($outputNode.contents.subarray(0, contentsSize));
+    // おしりを削って返す
+    return outputNode.contents.subarray(0, outputNode.size);
   }
 
   return {
